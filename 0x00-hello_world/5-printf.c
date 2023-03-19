51f776ddca7b429bffdb7ad19bc48e527c797230
Favour Michael <michaelfavour418@gmail.com>
2023-03-19 06:45:21 -0700

    Task 5. Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,7 +3,7 @@
  * main - A program that prints a line with printf not puts;
  * Return: 0 (Success);
  */
-int main(void);
+int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
