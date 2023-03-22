25e3085cf8fa527ea2be6bb197c2133f2f141af0
Favour Michael <michaelfavour418@gmail.com>
2023-03-22 10:58:00 -0700

    Alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints letters of alphabet in lower case with new line
+ * Return: 0 always
+ */
+void print_alphabet(void)
+{
+	char lc;
+
+	for (lc = 'a'; la <= 'z'; la++)
+		_putchar(lc);
+
+	_putchar('\n');
+	return (0);
+}
