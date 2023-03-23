2584f886eabab31f031f4de6b48001e3010bc8e1
Favour Michael <michaelfavour418@gmail.com>
2023-03-22 21:45:09 -0700

    Task 5. Signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,11 +7,28 @@
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
+/* Declaration of _putchar*/
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
 		return (1);
-	} else if (n == 0)
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
 }
