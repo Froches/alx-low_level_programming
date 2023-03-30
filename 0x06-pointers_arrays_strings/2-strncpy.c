164218f6ad753f0e305dd4e44cf470aaea75e414
Favour Michael <michaelfavour418@gmail.com>
2023-03-30 13:03:06 -0700

    Task 2. strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies an inputted number of bytes
+ * @dest: The buffer storing the string to be copied
+ * @src: The source string
+ * @n: The maximum no. bytes to be copied
+ *
+ * Return: A pointer to resulting string dest.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index = 0, src_len = 0;
+
+	while (src[index++])
+		src_len++;
+
+	for (inddex = 0; src[index] && index < n; index++)
+		dest[index] = '\0';
+
+	return (dest);
+}
