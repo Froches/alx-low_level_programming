2ea7f23f6e314a3b91135447b0812e1405bbfcfb
Favour Michael <michaelfavour418@gmail.com>
2023-04-06 22:12:48 -0700

    Task 5. square recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returns natural square root of a number
+ * @n: the number whose square root is to be found
+ *
+ * Return: the natural square root of the number
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (_sqrt_helper(n, 1, n));
+}
+
+/**
+ * _sqrt_helper - Recursive helper function to find the square root.
+ * @n: The number to find the square root of.
+ * @low: The lowest possible square root of n.
+ * @high: The highest possible square root of n.
+ *
+ * Return: The natural square root of the number.
+ */
+int _sqrt_helper(int n, int low, int high)
+{
+	int mid;
+
+	if (low > high)
+		return (-1);
+
+	mid = (low + high) / 2;
+	if (mid * mid == n)
+		return (mid);
+	else if (mid * mid > n)
+		return (_sqrt_helper(n, low, mid - 1));
+	else
+		return (_sqrt_helper(n, mid + 1, high));
+}
