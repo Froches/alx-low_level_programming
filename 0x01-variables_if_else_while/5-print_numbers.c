462bca658d83719ee476fc4e300b8ce9f25fc1ed
Favour Michael <michaelfavour418@gmail.com>
2023-04-07 07:23:23 -0700

    Task 1. Last digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - program that prints single digit numbers in base 10 with new line
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	if (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+
+	return (0);
+}
