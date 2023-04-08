28071e4b3185e11988c9627f497062439ffd8728
Favour Michael <michaelfavour418@gmail.com>
2023-04-08 04:37:20 -0700

    Task 10. Print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,8 +14,12 @@ int main(void)
 		{
 			putchar(j + '0');
 			putchar(k + '0');
-			putchar(',');
-			putchar(' ');
+
+			if (j != 8 || k != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
