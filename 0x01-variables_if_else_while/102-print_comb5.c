8aefb7dd64e11e4a7682a1949da9f699017a3438
Favour Michael <michaelfavour418@gmail.com>
2023-04-08 05:23:04 -0700

    Task 12. software is eating the world!

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digit numbers
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int p, q, j, k;
+
+	for (p = 0; p < 10; p++)
+	{
+		for (q = 0; q < 10; q++)
+		{
+			for (j = p; j < 10; j++)
+			{
+				for (k = (j == p ? j + 1 : 0); k <= 9; k++)
+				{
+					putchar(p + '0');
+					putchar(q + '0');
+					putchar(' ');
+					putchar(j + '0');
+					putchar(k + '0');
+
+					if (p != 9 || q != 8 || j != 9 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
