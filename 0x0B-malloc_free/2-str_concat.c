7b3bdedd997f99dc35d99f756d94abea927a9998
Favour Michael <michaelfavour418@gmail.com>
2023-04-13 07:27:53 -0700

    Task 2. concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,15 +10,22 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	size_t s1_len = strlen(s1);
-	size_t s2_len = strlen(s2);
-	char *result = malloc(s1_len + s2_len + 1);
+	size_t s1_len, s2_len;
+	char *result;
+
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
+
+	result = malloc(s1_len + s2_len + 1);
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
 	if (s2 == NULL)
+	{
+		s2 = "";
+	}
 
 	if (result == NULL)
 	{
