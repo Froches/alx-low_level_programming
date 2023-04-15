5a9ae9eeb223eec86b13fa71f5aa54592038e458
Favour Michael <michaelfavour418@gmail.com>
2023-04-15 09:34:11 -0700

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,32 +8,11 @@
 int print_last_digit(int num)
 {
 	int last_digit = num % 10;
-	int digit_char = last_digit + '0';
 
-	putchar('T');
-	putchar('h');
-	putchar('e');
-	putchar(' ');
-	putchar('l');
-	putchar('a');
-	putchar('s');
-	putchar('t');
-	putchar(' ');
-	putchar('d');
-	putchar('i');
-	putchar('g');
-	putchar('i');
-	putchar('t');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar(num + '0');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar(digit_char);
-	putchar('\n');
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+	putchar(last_digit + '0');
 	return (last_digit);
 }
