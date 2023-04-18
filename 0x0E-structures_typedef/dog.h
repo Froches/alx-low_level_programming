97cab4c47b0190d365a5028e2c054c9005147f72
Favour Michael <michaelfavour418@gmail.com>
2023-04-17 17:53:37 -0700

    Task 0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a struct containing information about a dog
+ * @name: name of the dog (string)
+ * @age: age of the dog (float)
+ * @owner: owner of the dog (string)
+ *
+ * Description: This struct contains information about a dog.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif	/* DOG_H */
