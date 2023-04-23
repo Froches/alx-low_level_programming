b2dcf4b2c054b61625100470f8a328772d1e6640
Favour Michael <michaelfavour418@gmail.com>
2023-04-23 06:05:51 -0700

    Task 10. add numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the sum
+ * @a: one of the ints to be added together
+ * @b: second int in equation
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c - a + b;
+
+	return (c);
+}
