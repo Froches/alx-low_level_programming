fdfa0862da7da2d266118fe9b27193d7d169e294
Favour Michael <michaelfavour418@gmail.com>
2023-04-24 03:16:12 -0700

    Task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,24 +8,38 @@
  */
 int _atoi(char *s)
 {
-	int sign = 1;
-	int num = 0;
-	int i = 0;
+	int i, d, n, len, f, digit;
 
-	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-		i++;
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
 
-	if (s[i] == '-' || s[i] == '+')
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
 	{
 		if (s[i] == '-')
-			sign = -1;
-		i++;
-	}
+		++d;
 
-	while (s[i] >= '0' && s[i] <= '9')
-	{
-		num = num * 10 + (s[i] - '0');
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
 		i++;
 	}
-	return (sign * num);
+	if (f == 0)
+		return (0);
+
+	return (n);
 }
