f0de376dc6fcf4198018b1030c451a30e5953ff2
Favour Michael <michaelfavour418@gmail.com>
2023-04-28 13:36:56 -0700

    Task 7. leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,7 +1,32 @@
 #include "main.h"
 /**
  * leet - encode into 1337speak
- * @n: input value
+ * @str: string
  * Return: n value
  */
+char *leet(char *str)
+{
+	int i, j;
+	char leet[5] = {'4', '3', '0', '7', '1'};
+	char letters[5][2] = {
+		{'a', 'A'},
+		{'e', 'E'},
+		{'o', 'O'},
+		{'t', 'T'},
+		{'l', 'L'}
+	};
 
+	for (i = 0; str[i]; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == letters[j][0] || str[i] == letters[j][1])
+			{
+				str[i] = leet[j];
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
