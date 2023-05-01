7a9779f2f8c8ad3d1b1a35c8e0596294ae8754dd
Favour Michael <michaelfavour418@gmail.com>
2023-05-01 05:00:58 -0700

    Task 3. calculator

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,64 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "3-calc.h"
 
 /**
  * main - Entry point
- * Return: 0
+ * @a: parameter for function
+ * @b: second parameter
+ *
+ * Return: Always 0
  */
+
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 
-int main(int argc, char *argv[]) {
-    int a, b;
-    int (*op_func)(int, int);
-    
-    if (argc != 4) {
-        printf("Error\n");
-        return 1;
-    }
-    
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    
-    if (argv[2][1] != '\0') {
-        printf("Error\n");
-        return 1;
-    }
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	int (*op_func)(int, int);
 
-    switch(argv[2][0]) {
-        case '+':
-            op_func = &op_add;
-            break;
-        case '-':
-            op_func = &op_sub;
-            break;
-        case '*':
-            op_func = &op_mul;
-            break;
-        case '/':
-            if (b == 0) {
-                printf("Error\n");
-                return 2;
-            }
-            op_func = &op_div;
-            break;
-        case '%':
-            if (b == 0) {
-                printf("Error\n");
-                return 2;
-            }
-            op_func = &op_mod;
-            break;
-        default:
-            printf("Error\n");
-            return 1;
-    }
-    
-    printf("%d\n", op_func(a, b));
-    
-    return 0;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		return (98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (op_func == NULL)
+	{
+		printf("Error\n");
+		return (99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		return (100);
+	}
+
+	result = op_func(num1, num2);
+	printf("%d\n", result);
+
+	return (0);
 }
-
