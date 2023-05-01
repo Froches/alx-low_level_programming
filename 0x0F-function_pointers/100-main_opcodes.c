d108368393380bcfdb82b9aefc03b5eb83a48a7e
Favour Michael <michaelfavour418@gmail.com>
2023-05-01 06:28:03 -0700

    Task 4. opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,23 +2,6 @@
 #include <stdlib.h>
 
 /**
- * print_opcodes - prints the opcodes of a function
- * @ptr: pointer to the function to print opcodes for
- * @bytes: number of bytes to print
- *
- * Return: void
- */
-void print_opcodes(unsigned char *ptr, int bytes)
-{
-	int i;
-
-	for (i = 0; i < bytes; i++)
-		printf("%02x ", *(ptr + i));
-
-	printf("\n");
-}
-
-/**
  * main - prints its own opcodes
  * @argc: number of arguments passed to the program
  * @argv: array of arguments passed to the program
@@ -28,24 +11,26 @@ void print_opcodes(unsigned char *ptr, int bytes)
  */
 int main(int argc, char **argv)
 {
-	int bytes;
+	int bytes, i;
 	unsigned char *main_ptr;
 
 	if (argc != 2)
 	{
-		printf("Usage: %s bytes\n", argv[0]);
+		printf("Error\n");
 		return (1);
 	}
 
 	bytes = atoi(argv[1]);
-	if (bytes <= 0)
+	if (bytes < 0)
 	{
-		printf("%s: Invalid number of bytes: %s\n", argv[0], argv[1]);
+		printf("Error\n");
 		return (2);
 	}
 
 	main_ptr = (unsigned char *)main;
-	print_opcodes(main_ptr, bytes);
+	for (i = 0; i < bytes; i++)
+		printf("%02x ", *(main_ptr + i));
 
+	printf("\n");
 	return (0);
 }
