248f49661ebe4798a4f791d8db1f954782d0f66b
Favour Michael <michaelfavour418@gmail.com>
2023-05-03 03:30:12 -0700

    print fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int first = 1, second = 2, next, i;
+
+	printf("%d, %d, ", first, second);
+
+	for (i = 2; i < 50; i++)
+	{
+		next = first + second;
+		printf("%d", next);
+		if (i < 49)
+		{
+			printf(", ");
+		}
+		first = second;
+		second = next;
+	}
+
+	printf("\n");
+
+	return (0);
+}
