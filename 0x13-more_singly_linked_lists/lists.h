a50883d7a574204d22394c3b9496d81258c9fc91
Favour Michael <michaelfavour418@gmail.com>
2023-05-02 17:30:41 -0700

    Task 10. Delete at index

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -27,5 +29,6 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
 int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *add_nodeint(listint_t **head, const int n);
 
 #endif
