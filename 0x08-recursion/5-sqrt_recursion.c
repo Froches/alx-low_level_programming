eefeecd3162e69350fae917f8fb733ed827c018d
Favour Michael <michaelfavour418@gmail.com>
2023-06-09 14:47:08 +0100

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,25 +2,33 @@
 #include "main.h"
 
 /**
+ sqrt_helper - helper function to calculate the square root
+ * @n: the number whose square root is to be calculated
+ * @guess: the current guess for the square root
+ * Return: the natural square root or -1 if n does not have a square root
+ */
+
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+		return (guess);
+
+	if (guess * guess > n)
+		return (-1);
+
+	return (sqrt_helper(n, guess + 1));
+}
+
+/**
  *_sqrt_recursion - returns natural square root of a number
- *@n: parameter
+ *@n: the number whose square root is to be calculated
  *Return: natural square root or -1
  */
 
 int _sqrt_recursion(int n)
 {
-	float result;
-
 	if (n < 0)
 		return (-1);
 
-	if (n == 0 || n == 1)
-		return (n);
-
-	result = _sqrt_recursion(n / 2);
-
-	while (fabs(result * result - n) > 0.001)
-		result = (result + n / result) / 2;
-
-	return ((int) result);
+	return (sqrt_helper(n, 0));
 }
