dae51a269f4c9537d77af5fcde6fefb21b34ed69
Favour Michael <michaelfavour418@gmail.com>
2023-06-20 03:11:41 +0100

    Task 2. Print struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - function that prints struct dog
+ * @d: struct variable
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		if (d->name != NULL)
+		{
+			printf("Name: %s\n", d->name);
+		}
+		else
+		{
+			printf("nil");
+		}
+		printf("Age: %.1f\n", d->age);
+		printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+	}
+}
