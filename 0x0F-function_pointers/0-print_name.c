e9f91f4301299c1fb67c9632491617bba430d292
Favour Michael <michaelfavour418@gmail.com>
2023-06-22 01:46:33 +0100

    Task 0. prnt name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stddef.h>
 
+/**
+ * print_name - function that prints a name
+ * @name: name of the person
+ * @f: pointer to function
+ */
+
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
