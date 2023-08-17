4a551a5eba2291daf0cd627481da410522155885
Froches <michaelfavour418@gmail.com>
2023-08-17 13:03:59 -0700

    Task 4. free list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *curr;
+
+	while (head != NULL)
+	{
+		curr = head;
+		head = head->next;
+		free(curr);
+	}
+}
