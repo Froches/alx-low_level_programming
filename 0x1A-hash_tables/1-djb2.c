7f98cb7d4d6e6e1d42f63a2a601c640d7b223cfc
Froches <michaelfavour418@gmail.com>
2023-09-08 16:12:00 -0700

    Hash function

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ * hash_djb2 - hash function
+ * @str: string parameter
+ *
+ * Return: hash
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
