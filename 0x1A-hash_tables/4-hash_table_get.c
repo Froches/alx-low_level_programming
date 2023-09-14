f99be69910b2139b45a9ac97744ffb9527830616
Froches <michaelfavour418@gmail.com>
2023-09-13 20:56:44 -0700

    Task 4. retrieve values

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,7 +1,27 @@
 #include "hash_tables.h"
-
+/**
+ * hash_table_get - function that retrieves a value assocoated with a key
+ * @ht: The hash table to look into
+ * @key: the key to look for
+ *
+ * Return: the value associated with the element or NULL if key not found
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
+{
 	hash_node_t *current_node;
 	unsigned long index;
 
-	of the
+	if (ht == NULL || key == NULL || *key == '\0')
+	return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	current_node = ht->array[index];
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
